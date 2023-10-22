0a4947b5539efaf978942f5b19c19d07bbb13dd9
Ali Baig Awan <77206379+aliawan739@users.noreply.github.com>
2023-10-22 22:16:31 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,38 @@
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+let score = "ali awan"
+
+
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
+
+
+//OUTPUTS
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+// undefined => NaN
+
+
+// let isLogedIn = 1
+// let isLogedIn = ""
+let isLogedIn = "ali awan"
+
+let booleanIsLoggedIn = Boolean(isLogedIn)
+// console.log(booleanIsLoggedIn);
+
+// OUTPUTS
+// 1 => true
+// 0 => false
+// "" => false
+// "ali awan" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
