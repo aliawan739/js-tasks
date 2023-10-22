2fb354a359370f444325dac2263ae95dd600cb63
Ali Baig Awan <77206379+aliawan739@users.noreply.github.com>
2023-10-22 21:40:58 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 1131
+let accountEmail = "ali@ali.com"
+var accountPassword = "password"
+accountCity = "shanghai"
+let accountState
+
+
+
+// accountId = 2 Not allowed
+
+accountEmail = "BAIG@ali.com"
+accountPassword = "121212"
+accountCity = "Lahore"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
