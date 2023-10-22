af8e313c6d275d0b810d05cc98e1595825977967
Ali Baig Awan <77206379+aliawan739@users.noreply.github.com>
2023-10-22 21:59:35 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code in this file as ES6
+
+// alert("Hello World!");  we are using Node JS, not browser
+
+let name = "ali awan"
+let age ="24"
+let isLoggedIn = false
+let state;
+
+
+// number => 2 to the power of 53
+// bigint 
+// string => " ali awan " use double quotes for string
+// bollean => true or false
+// NULL => it is a standlone value
+// undefined => it is
+// symbol => unique
+
+
+// objects
+
+console.log(typeof "ali awan");
